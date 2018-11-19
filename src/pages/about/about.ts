7c46850cbebf47c5bee3df621b23801545865c63
biswajitExicube <biswajitdebnath249@gmail.com>
2018-11-19 15:21:58 +0530

    Project Structure Done.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -19,34 +19,10 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController, public httpClient: HttpClient, public menuCtrl: MenuController) {
 
-    this.loadUsers();
   }
   openMenu(side){
     this.menuCtrl.toggle('right');    
   }
 
-  loadUsers(infiniteScroll?) {
-    this.httpClient.get(`https://randomuser.me/api/?results=20&page=${this.page}`)
-    .subscribe(res => {
-      this.users = this.users.concat(res['results']);
-      console.log(this.users);
-      for(let i = 0; i<this.users.length; i++){
-        this.dob = this.users[i].dob.date;
-      }
-      console.log(this.dob);
-      if (infiniteScroll) {
-        infiniteScroll.complete();
-      }
-    })
-  }
- 
-  loadMore(infiniteScroll) {
-    this.page++;
-    this.loadUsers(infiniteScroll);
- 
-    if (this.page === this.maximumPages) {
-      infiniteScroll.enable(false);
-    }
-  }
 
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -11,15 +11,6 @@ export class ContactPage {
 
   constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
 
-    // extend button functionality
-  //   $('#fs').bind('click', function() {
-  //     // display the video
-  //     $("#myVideo").css({'visibility' : 'visible', 'display' : 'block'});
-
-  //     // launch the video fullscreen
-  //     var vid = document.getElementById("myVideo");
-  //     vid.webkitEnterFullscreen();
-  // });
 
   }
 
@@ -27,15 +18,4 @@ export class ContactPage {
     this.menuCtrl.toggle('right');    
   }
 
-  launchIntoFullscreen(element) {
-    if(element.requestFullscreen) {
-      element.webkitEnterFullscreen();
-    } else if(element.mozRequestFullScreen) {
-      element.webkitEnterFullscreen();
-    } else if(element.webkitRequestFullscreen) {
-      element.webkitEnterFullscreen();
-    } else if(element.msRequestFullscreen) {
-      element.webkitEnterFullscreen();
-    }
-  }
 }
